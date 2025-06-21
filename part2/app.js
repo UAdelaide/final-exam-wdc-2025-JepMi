1769122d0ae31399aa5c48f091961face858dae1
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 03:43:12 +0000

    [automatic commit] 2025-06-21 03:43:12 UTC 49.178.202.188

--- part2/app.js
+++ part2/app.js
@@ -24,49 +24,6 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-// 🔌 Connect to MySQL
-let db;
-(async () => {
-  db = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'DogWalkService'
-  });
-})();
-
-// 🔐 Login Route
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const [users] = await db.execute(
-      'SELECT * FROM Users WHERE username = ? AND password_hash = ?',
-      [username, password]
-    );
-
-    if (users.length > 0) {
-      const user = users[0];
-
-      req.session.user = {
-        id: user.user_id,
-        username: user.username,
-        role: user.role
-      };
-
-      if (user.role === 'owner') {
-        return res.redirect('/owner-dashboard.html');
-      } if (user.role === 'walker') {
-        return res.redirect('/walker-dashboard.html');
-      }
-    } else {
-      res.send('Invalid username or password');
-    }
-  } catch (err) {
-    console.error('Login failed:', err);
-    res.status(500).send('Something went wrong');
-  }
-});
 
 
 // Export the app instead of listening here
