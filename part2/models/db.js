c72836649234334b53e5e9de776c77d3739176e0
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 06:31:23 +0000

    [automatic commit] 2025-06-21 06:31:23 UTC ;; communications error to 208.67.222.222#53: timed out 49.178.202.188

--- part2/models/db.js
+++ part2/models/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
+  socketPath: '/var/run/mysqld/mysqld.sock',
   user: 'root',
-  password: 'michele123', // 🔐 <- add this line
+  password: 'michele123',
   database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
