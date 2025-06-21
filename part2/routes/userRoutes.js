bd4c6bb48e63a0ac6765f07bb784673b8a26d55b
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 06:02:44 +0000

    [automatic commit] 2025-06-21 06:02:44 UTC 49.178.202.188

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
-const bcrypt = require('bcryptjs');
 
 
 // GET all users (for admin/testing)
