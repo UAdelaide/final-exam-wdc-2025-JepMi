608f635488ce20a9b029af53585937a1568c34d6
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 03:32:38 +0000

    [automatic commit] 2025-06-21 03:32:38 UTC 49.178.202.188

--- starthere/routes/index.js
+++ starthere/routes/index.js
@@ -6,17 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
 router.post('/logout', (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      console.log('Logout error:', err)
-      return res.redirect('/')
+      console.log('Logout error:', err);
+      return res.redirect('/');
     }
 
-    res.clearCookie('connect.sid')
-    return res.redirect('/')
-  })
-})
+    res.clearCookie('connect.sid');
+    return res.redirect('/');
+  });
+});
 
 
 module.exports = router;
