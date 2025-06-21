94eeaeb2417c9ba682a8a60663ea47c4e739a4c2
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 05:56:35 +0000

    [automatic commit] 2025-06-21 05:56:35 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 49.178.202.188

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
+const bcrypt = require('bcryptjs');
+
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
